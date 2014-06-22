e15f71379ca2f4e004032a35b8140def98895f32
Mikedgs <michael.douglas21@gmail.com>
2014-06-23 11:57:12 +1200

    Added shell of project

---
+++ Quoridors/Quoridors/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quoridors.MvcApplication" Language="C#" %>
---
+++ Quoridors/Quoridors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quoridors.Startup))]
+namespace Quoridors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
