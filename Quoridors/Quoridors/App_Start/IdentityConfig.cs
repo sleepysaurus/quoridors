39f2fa58739bff8133e8fd5755da9fc3a471630e
Sleepysaurus <ylegaspi@gmail.com>
2014-06-23 14:17:06 +1200

    Added partial view, resolved nuget issues, built project

--- Quoridors/Quoridors/App_Start/IdentityConfig.cs
+++ Quoridors/Quoridors/App_Start/IdentityConfig.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
-using Quoridors.Models;
+//using Quoridors.Models;
 
 namespace Quoridors
 {
