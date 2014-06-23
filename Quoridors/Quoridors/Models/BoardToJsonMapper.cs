e10ce242748e7aa8d51fa5f9366a848f148a0edd
Ninjah45 <Ben.tehira@gmail.com>
2014-06-24 09:17:07 +1200

    Pushing branch

---
+++ Quoridors/Quoridors/Models/BoardToJsonMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quoridors.Models
+{
+    public class BoardToJsonMapper
+    {
+        public BoardJson MapBoardToJson(string[][] board)
+        {
+            
+        }
+    }
+}
