4f40c2a227dc0dc7bc0ba3d249d7f67ec7404375
Ninjah45 <Ben.tehira@gmail.com>
2014-06-24 14:18:42 +1200

    Completed game factory, need ninject

--- Quoridors/Quoridors/Models/GameFactory.cs
+++ Quoridors/Quoridors/Models/GameFactory.cs
@@ -1,14 +1,17 @@
-﻿using Quoridors.Models.Database;
+﻿using Quoridors.Models.Database.Interfaces;
 
 namespace Quoridors.Models
 {
     public class GameFactory
     {
         private readonly IBoardStateUpdater _boardStateUpdater;
+        private readonly IGameRepository _gameRepository;
+        private readonly GameDbMapperToGame _dbMapperToGame = new GameDbMapperToGame();
 
-        public GameFactory(IBoardStateUpdater boardStateUpdater)
+        public GameFactory(IBoardStateUpdater boardStateUpdater, IGameRepository gameRepository)
         {
             _boardStateUpdater = boardStateUpdater;
+            _gameRepository = gameRepository;
         }
 
         public Game New()
@@ -18,8 +21,8 @@ namespace Quoridors.Models
 
         public Game Load(int gameId)
         {
-             var game = new GameRepository().GetById(gameId); // TODO BA move this to an IGameRepository on the constructor 
-            // TODO map from GameDb to Game
+            var gameDb = _gameRepository.GetById(gameId);
+            var game = _dbMapperToGame.MappingGameFromDatabase(gameDb);
             _boardStateUpdater.UpdateBoardToSavedState(game);
             return game;
         }
