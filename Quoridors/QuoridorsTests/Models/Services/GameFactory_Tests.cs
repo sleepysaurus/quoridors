87f4ba88f56e29df3733199ce00370255827a272
regan-young <im.regan.young@gmail.com>
2014-06-24 17:24:24 +1200

    Updated some stuff

--- Quoridors/QuoridorsTests/Models/Services/GameFactory_Tests.cs
+++ Quoridors/QuoridorsTests/Models/Services/GameFactory_Tests.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
+using Quoridors.Controllers;
+using Quoridors.Models.Database;
+using Quoridors.Models.Database.Interfaces;
+using Quoridors.Models.Services;
 
 namespace QuoridorsTests.Models.Services
 {
@@ -14,11 +19,14 @@ namespace QuoridorsTests.Models.Services
         public void The_new_method_emits_the_expected_array()
         {
             // Arrange
+            var gameRepo = Mock.Of<IGameRepository>();
+            var gamefactory = new GameFactory(null, gameRepo, null);
 
             // Act
+            var newgame = gamefactory.New();
 
             // Assert
-
+            Assert.That(newgame.Board.Length == 17);
         }
     }
 }
