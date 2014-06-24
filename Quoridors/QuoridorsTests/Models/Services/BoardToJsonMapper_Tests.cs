803c6505acde59ad7bcb0783472ea75e5cc3939d
Ninjah45 <Ben.tehira@gmail.com>
2014-06-25 11:11:41 +1200

    Added tests to boardToJsonMapper

--- Quoridors/QuoridorsTests/Models/Services/BoardToJsonMapper_Tests.cs
+++ Quoridors/QuoridorsTests/Models/Services/BoardToJsonMapper_Tests.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
+using Quoridors.Models;
+using Quoridors.Models.Database;
+using Quoridors.Models.DatabaseModels;
+using Quoridors.Models.Services;
 
 namespace QuoridorsTests.Models.Services
 {
@@ -11,30 +12,80 @@ namespace QuoridorsTests.Models.Services
     public class BoardToJsonMapper_Tests
     {
         [Test]
-        public void Given_a_valid_input_the_mapper_returns_the_expected_output()
+        public void Given_a_valid_input_the_mapper_returns_the_expected_output_from_the_CreateBoardObject_method()
         {
             // Arrange
-            
+            var mock = new Mock<IPositionRepository>();
+            var cut = new BoardToJsonMapper(mock.Object);
+            var game = new Game();
 
             // Act
+            var result = cut.CreateBoardObject(game.Board, game);
 
             // Assert
+            Assert.IsInstanceOf(typeof(BoardToJson), result);
 
         }
 
-        // TODO what happens if you pass in an invalidly sized array? Can you pass a non-primitive to GetListOfBricks and design that problem out?
-        // TODO what happens if you stick invalid values in the array?
+        [Test]
+        public void GetListOfBricks_returns_a_valid_output()
+        {
+            // Arrange
+            var game = new Game();
+            var cut = new BoardToJsonMapper(null);
 
+            // Act
+            var result = cut.GetListOfBricks(game.Board);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(List<Brick>), result);
+        }
+
+        [Test]
+        public void GetListOfBricks_returns_correct_number_of_bricks_in_list_for_given_board()
+        {
+            // Arrange
+            var game = new Game();
+            game.Board[6][7] = BoardCellStatus.Wall;
+            game.Board[8][7] = BoardCellStatus.Wall;
+            var cut = new BoardToJsonMapper(null);
+
+            // Act
+            var result = cut.GetListOfBricks(game.Board);
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetListOfBricks_returns_correct_list_for_given_board()
+        {
+            // Arrange
+            var game = new Game();
+            game.Board[6][7] = BoardCellStatus.Wall;
+            var cut = new BoardToJsonMapper(null);
+
+            // Act
+            var result = cut.GetListOfBricks(game.Board)[0];
+
+            // Assert
+            Assert.That(result.XPos, Is.EqualTo(3));
+            Assert.That(result.YPos, Is.EqualTo(4));
+        }
 
         [Test]
         public void GetListOfPlayerPositions_returns_the_value_from_the_repository_mapped_correctly()
         {
             // Arrange
-            // TODO create a mock repo, set it up to return data
+            var mock = new Mock<IPositionRepository>();
+            mock.Setup(x => x.All()).Returns(new List<PositionDb> {new PositionDb(1,2,3,1)});
+            var cut = new BoardToJsonMapper(mock.Object);
 
             // Act
-
+            var result = cut.GetListOfPlayerPositions();
             // Assert
+            Assert.IsInstanceOf(typeof(List<PositionJson>), result);
+            mock.Verify(x => x.All(), Times.Exactly(1));
 
         }
     }
