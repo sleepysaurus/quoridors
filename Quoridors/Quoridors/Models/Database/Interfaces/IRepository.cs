07c439c367c9e2c5f46a9c99a1e654f3519b2aac
regan-young <im.regan.young@gmail.com>
2014-06-27 15:00:31 +1200

    Connected front end and back end woop

--- Quoridors/Quoridors/Models/Database/Interfaces/IRepository.cs
+++ Quoridors/Quoridors/Models/Database/Interfaces/IRepository.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace Quoridors.Models.Database.Interfaces
 {
-    public interface IRepository<T> : IDisposable
+    public interface IRepository<T>
     {
         IEnumerable<T> All();
         void ExecuteStoredProcedure(string procedureName, T thingToDoStuffWith, SqlParameter[] parameters);
         IEnumerable<T> ExecuteReadStoredProcedure(string procedureName, SqlParameter[] parameters);
         void ExecuteNonQuery(string query);
-        int GetLastId();
         IEnumerable<T> ExecuteRead(string query);
         T NewModel(SqlDataReader reader);
     }
